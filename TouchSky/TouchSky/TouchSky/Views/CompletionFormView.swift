9c74db3f0dc847952fcbcd84441971a40e562363
anhugenberg <anhugenberg@gmail.com>
2022-12-05 13:28:01 -0700

    first implementation of completion form persistence

--- TouchSky/TouchSky/TouchSky/Views/CompletionFormView.swift
+++ TouchSky/TouchSky/TouchSky/Views/CompletionFormView.swift
@@ -13,37 +13,49 @@ struct CompletionFormView: View {
     @State var timeValue: String = ""
     @State var journalEntry: String = ""
     
+    @State var text: String = UserDefaults.standard.string(forKey: "TEXT_KEY") ?? ""
+    @State var inputText: String = ""
+    
     var body: some View {
-        NavigationView{
-            VStack{
-                Form{
-                    Section{
-                        DatePicker(selection: $dateValue, in: ...Date(), displayedComponents: .date) {
-                                Text("Completion Date")
-                            }
-                    }
-                    Section{
-                        DatePicker(selection: $dateValue, in: ...Date(), displayedComponents: .hourAndMinute) {
-                                Text("Completion Time")
-                            }
-                    }
-                    Section{
-                        TextField("Jornal Entry:", text: $journalEntry)
-                    }
-                }
-            }
-            //.navigationTitle("Completion Form")
-            .toolbar{
-                ToolbarItem(placement: .confirmationAction){
-                    Button("Done"){
-                        
-                    }
-                }
-//                ToolbarItem(placement: .navigationBarLeading){
-//                    Button("Cancel"){
+//        NavigationView{
+//            VStack{
+//                Form{
+//                    Section{
+//                        DatePicker(selection: $dateValue, in: ...Date(), displayedComponents: .date) {
+//                                Text("Completion Date")
+//                            }
+//                    }
+//                    Section{
+//                        DatePicker(selection: $dateValue, in: ...Date(), displayedComponents: .hourAndMinute) {
+//                                Text("Completion Time")
+//                            }
+//                    }
+//                    Section{
+//                        TextField("Jornal Entry:", text: $journalEntry)
+//                    }
+//                }
+//            }
+//            .toolbar{
+//                ToolbarItem(placement: .confirmationAction){
+//                    Button("Done"){
 //
 //                    }
 //                }
+//            }
+//        }
+        Form {
+            Section(header: Text("date, time, stories, etc")){
+                TextField("Add some text here.", text: $inputText)
+            }
+            Button("Save"){
+                UserDefaults.standard.set(inputText, forKey: "TEXT_KEY")
+                text = inputText
+                print("Saved: \(inputText)")
+            }
+            .foregroundColor(Color.blue)
+            .frame(maxWidth: .infinity, alignment: .center)
+            Section(header: Text("Saved Entry:")){
+                Text(text)
             }
         }
     }
