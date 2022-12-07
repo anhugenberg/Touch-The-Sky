37072d15eacc570a59089b0085972b4a6f6035da
anhugenberg <anhugenberg@gmail.com>
2022-12-06 22:00:02 -0700

    Revert "edit to show launch screen for 2 seconds when launching app"
    This reverts commit 3eeeccbb02e70f64ffc75c34fa6223391613b652.

--- TouchSky/TouchSky/TouchSky/TouchSkyApp.swift
+++ TouchSky/TouchSky/TouchSky/TouchSkyApp.swift
@@ -10,23 +10,11 @@ import SwiftUI
 @main
 struct TouchTheSkyApp: App {
     @StateObject private var modelData = ModelData()
-    @State var showLaunch: Bool = true
     
     var body: some Scene {
         WindowGroup {
-            if showLaunch {
-                LaunchScreenView()
-                    .onAppear() {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                            self.showLaunch = false
-                        }
-                    }
-
-            } else {
-                HomeView()
-                    .environmentObject(modelData)
-
-            }
+            HomeView()
+                .environmentObject(modelData)
         }
     }
 }
